cb15095b24b46ecceae2d29752b4a6c3a5d99e6e
Cmbs86 <carlosmbs86@outlook.ie>
2025-12-24 19:52:09 +0100

    add google fonts

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './index.css'
 export default function App() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-second-pink">
-      <h1 className="text-6xl font-bold text-primary-green">
+      <h1 className="text-6xl font-bold text-primary-green font-permanent-marker">
        Appleseed 🚀
       </h1>
     </div>
