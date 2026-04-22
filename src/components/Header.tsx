f6164449420fdf99556f46f698485b1500b0781d
Cmbs86 <carlosmbs86@outlook.ie>
2026-04-22 19:29:18 +0200

    adjust positioninig main logo and add shadow to it

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -69,7 +69,7 @@ const Header = () => {
         <div className="max-w-[96%] mx-auto flex items-center justify-between">
           
           {/* Logo wrapper (ORIGINAL POSITIONING RESTORED) */}
-          <div className="flex w-32 h-32 md:w-36 md:h-32 lg:w-52 lg:h-36 justify-around items-center">
+          <div className="flex w-32 h-32 md:w-36 md:h-32 lg:w-64 lg:h-40 justify-around items-center">
             <Link
               to="/"
               className="flex items-center transition-all duration-700 ease-in-out"
@@ -77,8 +77,8 @@ const Header = () => {
               <img
                 src="/logos/riot-seed-color.svg"
                 alt="Riot Seed"
-                className={`w-auto object-contain transition-all duration-700 ease-in-out ${
-                  isScrolled ? "h-26" : "h-34"
+                className={`w-auto object-contain transition-all duration-700 ease-in-out drop-shadow-[5px_2px_0px_#000000] ${
+                  isScrolled ? "h-26" : "h-36"
                 }`}
               />
             </Link>
